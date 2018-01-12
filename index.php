934b5a880ee8e3c26513a752b926a3d208348cab
Toygan Sevim <tsevim@mail.greenriver.edu>
2018-01-12 17:47:27 -0500

    added a defualt route and a .htaccess file to manage

--- index.php
+++ index.php
@@ -21,8 +21,39 @@ $f3->route('GET /', function (){
 
 });
 
+//page 1 route creation
+$f3->route('GET /page1', function (){
+
+    echo '<h1>This is page 1 </h1>';
+    //$view = new View;
+    // echo $view->render('views/home.html');
+
+});
+
+//page 1 route creation
+$f3->route('GET /page2', function (){
+
+    echo '<h1>This is page 2 </h1>';
+    //$view = new View;
+    // echo $view->render('views/home.html');
+
+});
+
+//a sub directory of the page 1
+//page 1 route creation
+$f3->route('GET /page1/subpage-a', function (){
+
+    echo '<h1>This is a subpage A, under page 1 </h1>';
+    //$view = new View;
+    // echo $view->render('views/home.html');
+
+});
+
+
+
+
+//fatfree enable error reporting
+$f3->set('DEBUG',3); // highest is 3 lowest 0;
 
 //run fat free
 $f3->run();
-
-?>
